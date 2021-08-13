777296126f9cc2c9504add9d34b284b84b4f65b9
Andrii Skega <Game.a.s.production@gmail.com>
2021-08-13 11:41:54 +0200

    Data types, operators, methods and properties

--- script.js
+++ script.js
@@ -1,15 +1,67 @@
-let money;
+// Variables 
 
-let income;
+let money = 1000;
 
-let addExpenses; 
+let income = "freelance";
 
-let deposit;
+let addExpenses = "internet, taxi, flat"; 
 
-let mission;
+let deposit = true;
 
-let period;
+let mission = Infinity;
 
-alert("Hello World!!!");
+let period = 2;
 
-console.log("Hello World!!!");
+alert("Are u ready?!");
+
+console.log("Hello curator!");
+
+
+
+// Methods & Properties
+
+console.log(typeof money, typeof income, typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log(("Период равен " +  period + " месяца.") + " " + ("Цель заработать " + mission + " долларов."));
+
+console.log(addExpenses.toLowerCase().split(", "));
+
+let budgetDay = money/30; 
+
+console.log(budgetDay);
+
+
+
+
+
+
+/* 
+
+2) Используя методы и свойства:
+
+
+
+   - Вывести в консоль тип данных значений переменных money, income, deposit;
+
+   - Вывести в консоль длину строки addExpenses
+
+   - Вывести в консоль “Период равен (period) месяцев” и “Цель заработать (mission) рублей/долларов/гривен/юани”
+
+   - Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
+
+   - Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30)
+
+   - Вывести в консоль budgetDay
+
+
+
+
+
+3) Проверить, чтобы все работало и не было ошибок в консоли
+
+
+
+4) Добавить папку или ветку со вторым уроком в свой репозиторий на GitHub
+*/
